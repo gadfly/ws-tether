6f28323e8aed6796ed492fac06f0f4ca03388b41
Crane Chen <cchen@rivernorth.com>
2017-05-01 15:30:46 -0500

    add config.js

---
+++ local-proxy/public/javascripts/config.js
@@ -0,0 +1,7 @@
+(function(global){
+  var config = {
+    localProxy : "ws://" + window.location.host
+    ,serverProxy : "ws://my-ws-iso-websurf.herokuapp.com"
+  }
+  global.config = config;
+})(this);
